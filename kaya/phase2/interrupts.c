26a474d7fa529738f3e9dee1a7e442bf58521830
Scott <scottpatharrington@gmail.com>
2019-10-13 13:14:57 -0400

    finished interrupt handler

--- kaya/phase2/interrupts.c
+++ kaya/phase2/interrupts.c
@@ -5,10 +5,12 @@ void InterruptHandler()
 	int i = 0;				/* loop control variable for determining interrupt line number */
 	int foundLine = FALSE; 	/* indicates whether or not the interrupt line has been found */
 	int lineNum; 			/* the highest priority line with interrupt */
+	int transmitBool = TRUE; /* assuming true because otherwise if its the othercase, we would want to change it to false*/
 	
 	/* find highest priority line with interrupt */
 	while(i < NUMLINES && !foundLine)
 	{
+		causeReg= causeReg & MASKCAUSEREG; /*  Zero out the irrelevant values */
 		unsigned int interruptOn = causeReg & INTERRUPTLINES[i]; /* will be all 0s if there is NOT an interrupt on line i */
 		/* line i has an interrupt */
 		if(interruptOn != 0)
@@ -45,6 +47,7 @@ void InterruptHandler()
 			/* find highest priority device with interrupt */
 			while(j < NUMDEVICES && !foundDevice)
 			{
+				devBitMap = devBitMap & MASKDEVBITMAP; /* Zeroing out irrelevant bits*/
 				unsigned int interruptOn = devBitMap & INTERRUPTDEVICES[j]; /* will be all 0s if there is NOT an interrupt on device i */
 				/* device i has an interrupt */
 				if(interruptOn != 0)
@@ -64,6 +67,48 @@ void InterruptHandler()
 			else
 			{
 				device_t* deviceAddr = (device_t*) (BASEDEVICEADDRESS + ((lineNum - INITIALDEVLINENUM) * DEVICETYPESIZE) + (deviceNum * DEVICESIZE));
+				int index = (lineNum - INITIALDEVLINENUM) * NUMDEVICESPERTYPE + deviceNum; /* calcuating index*/
+				if(lineNum ==7)
+					{
+						if(deviceAddr->t_transm_status == 0x00000001)
+						{
+							index += NUMDEVICESPERTYPE /* its a receive!*/
+							transmitBool = FALSE;
+						} 
+						/* else we do nothing as its a trans-status*/
+					}
+				int * Sema4 = &(semaphoreArray[index]);
+				(*Sema4)++;
+				if((*Sema4) <= 0)
+					{
+						pcb_ptr Proc=removeBlocked(Sema4);
+						Proc->p_s->s_v0 = deviceAddr->d_status;
+						softBlockCount--;
+						insertProcQ(&readyQ, Proc);
+						if(lineNum ==7 && transmitBool)
+							{
+								deviceAddr->d_transm_command = 0x00000001; 
+							}	
+							else
+							{
+								deviceAddr->d_command= 0x00000001; /* put this in const*/
+							}
+						if(currentProcess == NULL)
+							{
+								Scheduler();  /* want to call scheduler if current process is null*/
+							}
+						else
+							{
+								LoadState(interruptOld); /* otherwise we want to return control to the process*/
+							}
+
+
+						
+
+
+
+					}
+
 
 			}
 		}
