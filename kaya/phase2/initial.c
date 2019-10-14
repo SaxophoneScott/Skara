cd8f952add9a34b95fe38ee57d8dff1dc9d382a1
schatzk <36019128+schatzkara@users.noreply.github.com>
2019-10-14 15:33:47 -0400

    casting arguments

--- kaya/phase2/initial.c
+++ kaya/phase2/initial.c
@@ -30,10 +30,10 @@ void main(){
 
 	unsigned int statusRegValue = ALLOFF | VMOFF | KERNELON | INTERRUPTSMASKED; /* represents VM off, interrupts masked, and kernel mode on */
 
-	initializeNewArea(state_PTR SYSCALLNEWAREA, memaddr SyscallHandler, memaddr ramtop, unsigned int statusRegValue);
-	initializeNewArea(state_PTR PROGRAMTRAPNEWAREA, memaddr ProgramTrapHandler, memaddr ramtop, unsigned int statusRegValue);
-	initializeNewArea(state_PTR TLBMANAGEMENTNEWAREA, memaddr TLBManagementHandler, memaddr ramtop, unsigned int statusRegValue);
-	initializeNewArea(state_PTR INTERRUPTNEWAREA, memaddr InterruptHandler, memaddr ramtop, unsigned int statusRegValue);
+	initializeNewArea((state_PTR) SYSCALLNEWAREA, (memaddr) SyscallHandler, (memaddr) ramtop, statusRegValue);
+	initializeNewArea((state_PTR) PROGRAMTRAPNEWAREA, (memaddr) ProgramTrapHandler, (memaddr) ramtop, statusRegValue);
+	initializeNewArea((state_PTR) TLBMANAGEMENTNEWAREA, (memaddr) TLBManagementHandler, (memaddr) ramtop, statusRegValue);
+	initializeNewArea((state_PTR) INTERRUPTNEWAREA, (memaddr) InterruptHandler, (memaddr) ramtop, statusRegValue);
 
 	/* initialize data structures */
 	initPcbs();
@@ -71,9 +71,9 @@ HIDDEN void initializeNewArea(state_PTR memArea, memaddr handlerName, memaddr sp
 {
 	/* initialize the new area */
 	/* set PC to handler function */
-	state_PTR newMemArea = (state_PTR) memArea;
-	newMemArea->s_pc = (memaddr) handlerName; /* in exceptions.c */
-	newMemArea->s_t9 = (memaddr) handlerName; /* always set t9 to be the same as pc */
+	state_PTR newMemArea = memArea;
+	newMemArea->s_pc = handlerName; /* in exceptions.c */
+	newMemArea->s_t9 = handlerName; /* always set t9 to be the same as pc */
 	/* set stack pointer to last page of physcial memory (RAMTOP) */
 	newMemArea->s_sp = sp;
 	/* set status reg: VM off, Interrupts masked, Supervisor mode on */
