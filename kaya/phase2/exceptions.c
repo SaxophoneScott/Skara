21e4cae996c054da2d6cad687f808f405614a0bd
schatzk <36019128+schatzkara@users.noreply.github.com>
2019-10-21 16:16:24 -0400

    prints in exceptions

--- kaya/phase2/exceptions.c
+++ kaya/phase2/exceptions.c
@@ -8,6 +8,81 @@
 #include "../e/scheduler.e"
 #include "/usr/local/include/umps2/umps/libumps.e"
 
+char okbuf[2048];			
+#define TRANSMITTED	5
+#define ACK	1
+#define PRINTCHR	2
+#define CHAROFFSET	8
+#define STATUSMASK	0xFF
+#define	TERM0ADDR	0x10000250
+
+typedef unsigned int devreg;
+
+/* This function returns the terminal transmitter status value given its address */ 
+devreg termstat(memaddr * stataddr) {
+	return((*stataddr) & STATUSMASK);
+}
+
+/* This function prints a string on specified terminal and returns TRUE if 
+ * print was successful, FALSE if not   */
+unsigned int termprint(char * str, unsigned int term) {
+	memaddr * statusp;
+	memaddr * commandp;
+	devreg stat;
+	devreg cmd;
+	unsigned int error = FALSE;
+	
+	if (term < DEVPERINT) {
+		/* terminal is correct */
+		/* compute device register field addresses */
+		statusp = (devreg *) (TERM0ADDR + (term * DEVREGSIZE) + (TRANSTATUS * DEVREGLEN));
+		commandp = (devreg *) (TERM0ADDR + (term * DEVREGSIZE) + (TRANCOMMAND * DEVREGLEN));
+		
+		/* test device status */
+		stat = termstat(statusp);
+		if (stat == READY || stat == TRANSMITTED) {
+			/* device is available */
+			
+			/* print cycle */
+			while (*str != EOS && !error) {
+				cmd = (*str << CHAROFFSET) | PRINTCHR;
+				*commandp = cmd;
+
+				/* busy waiting */
+				stat = termstat(statusp);
+				while (stat == BUSY)
+					 stat = termstat(statusp);
+				
+				/* end of wait */
+				if (stat != TRANSMITTED)
+					error = TRUE;
+				else
+					/* move to next char */
+					str++;
+			} 
+		}
+		else
+			/* device is not available */
+			error = TRUE;
+	}
+	else
+		/* wrong terminal device number */
+		error = TRUE;
+
+	return (!error);		
+}
+
+
+/* This function placess the specified character string in okbuf and
+*	causes the string to be written out to terminal0 */
+void addokbuf(char *strp) {
+	char *tstrp = strp;
+	while ((*mp++ = *strp++) != '\0')
+		;
+	mp--;
+	termprint(tstrp, 0);
+}
+
 HIDDEN void CreateProcess(state_PTR syscallOld, state_PTR newState);
 HIDDEN void TerminateProcess(state_PTR syscallOld, pcb_PTR process);
 HIDDEN void HoneyIKilledTheKids(state_PTR syscallOld, pcb_PTR p);
@@ -31,6 +106,16 @@ void SyscallHandler(){
 	/*unsigned int l_v0;
 	unsigned int l_v1;*/
 	int userMode = currentProcess->p_s.s_status & KERNELOFF;
+	if(userMode == 0) /* in kernel mode */
+	{
+		userMode = FALSE;
+		addokbuf("in kernel mode \n")
+	}
+	else
+	{
+		userMode = TRUE;
+		addokbuf("in user mode \n")
+	}
 	syscallOld->s_pc += 4;
 	if(l_a0 > 8 || (!userMode && 1 <= l_a0 && l_a0 <= 8))
 	{
