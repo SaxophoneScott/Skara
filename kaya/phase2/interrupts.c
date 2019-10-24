c1409b2f42dde6a8093f43a2ec7ae85adf08fc68
Kara Schatz <schatzk@cerebro.cs.xu.edu>
2019-10-24 15:56:32 -0400

    some interrupt refactoring

--- kaya/phase2/interrupts.c
+++ kaya/phase2/interrupts.c
@@ -8,11 +8,14 @@
 #include "../e/scheduler.e"
 
 HIDDEN int DetermineLine(unsigned int causeReg);
+HIDDEN int DetermineDevice(unsigned int devBitMap);
+HIDDEN void PLTInterruptHandler();
+HIDDEN void ITInterruptHandler();
+HIDDEN void ExitInterruptHandler(state_PTR interruptOld);
 
 void InterruptHandler()
 {
 	cpu_t INTERRUPTSTART;											/* start time of interrupt handler */
-	cpu_t INTERRUPTEND;												/* end time of interrupt handler */
 	STCK(INTERRUPTSTART);
 	state_PTR interruptOld = (state_PTR) INTERRUPTOLDAREA;			/* state of the interrupted process */
 	unsigned int causeReg = interruptOld->s_cause;
@@ -25,59 +28,28 @@ void InterruptHandler()
 	/* case: it's the proccesor local timer */
 	if(lineNum == PLTLINE)
 	{
-		/* end the current process's turn */
-		IncrementProcessTime(currentProcess);
-		insertProcQ(&readyQ, currentProcess);
-		Scheduler();												/* schedule a new process to run */
-
+		PLTInterruptHandler();
 	}
 	/* case: it's the interval timer */
 	if(lineNum == ITLINE)
 	{
-		int* sema4 = &(semaphoreArray[ITSEMINDEX]);					/* interval timer's semaddr */
-		pcb_PTR p;
-		/* unblock all the processes that were waiting for the clock */
-		while((*sema4) < 0)
-		{
-			(*sema4)++;
-			p = removeBlocked(sema4);
-			insertProcQ(&readyQ, p);
-		}
-		(*sema4) = 0;
-		LDIT(INTERVALTIME);
+		ITInterruptHandler();
 	}
 	/* case: it's a device line */
 	else
 	{
 		devregarea_t* busRegArea = (devregarea_t*) RAMBASEADDR;
 		unsigned int devBitMap = busRegArea->interrupt_dev[lineNum - INITIALDEVLINENUM]; /* dev bit map for the line with an interrupt */
-		int j = 0; 													/* loop control variable for determining interrupt device number */
-		unsigned int deviceOn = DEVICE0;							/* indicates an interrupt on device i */
-		unsigned int interruptOn;
-		int foundDevice = FALSE; 									/* true if the interrupting device has been found, false otherwise */
-		int devNum;													/* highest priority device with interrupt */
+		int devNum;
 		device_t* deviceAddr;										/* address for device with interrupt */
 		int index;													/* index of the device's sema4 */
 		int * Sema4;												/* device's sema4 */
 
-		/* find highest priority device with interrupt */
-		while(j < NUMDEVICES && !foundDevice)
-		{
-			devBitMap = devBitMap & MASKDEVBITMAP; 					/* zero out irrelevant bits */
-			interruptOn = devBitMap & deviceOn; 					/* all 0s if there is NOT an interrupt on device i */
 
-			/* case: device i has an interrupt */
-			if(interruptOn != 0)
-			{
-				foundDevice = TRUE;
-				devNum = j;
-			}
-			j++; 													/* check the next device */
-			deviceOn = deviceOn << 1;
-		}
+		devNum = DetermineDevice(devBitMap);
 
 		/* one of the devices had an interrupt, so let's handle it */
-		deviceAddr = (device_t*) (BASEDEVICEADDRESS + ((lineNum - INITIALDEVLINENUM) * DEVICETYPESIZE) + (devNum * DEVICESIZE)); 
+		deviceAddr = (device_t*) (BASEDEVICEADDRESS + ((lineNum - INITIALDEVLINENUM) * DEVICETYPESIZE) + (devNum * DEVICESIZE));
 		index = (lineNum - INITIALDEVLINENUM) * NUMDEVICESPERTYPE + devNum; /* calcuating index */
 		/*	deviceAddr = (device_t *)( busRegArea->devreg[index]);  should be the same as the calcuation as before, emphasis on should*/
 
@@ -109,7 +81,7 @@ void InterruptHandler()
 					/* case: it's a transmit */
 					if(transmitBool)
 					{
-						proc->p_s.s_v0 = deviceAddr->t_transm_status; 
+						proc->p_s.s_v0 = deviceAddr->t_transm_status;
 					}
 					/* case: it's a receive */
 					else
@@ -153,17 +125,7 @@ void InterruptHandler()
 	}
 
 	/* we're done here, so let's get another process up and running */
-	/* case: there was no process that got interrupted */
-	if(currentProcess == NULL)
-	{
-		Scheduler();
-	}											/* schedule a new process to run */
-	/* case: a process got interrupted */
-	else
-	{
-		STCK(INTERRUPTEND);
-		LoadState(interruptOld); 									/* return control to the interrupted process*/
-	}
+	ExitInterruptHandler(interruptOld);
 }
 
 HIDDEN int DetermineLine(unsigned int causeReg)
@@ -191,4 +153,71 @@ HIDDEN int DetermineLine(unsigned int causeReg)
 	}
 
 	return lineNum;
+}
+
+HIDDEN int DetermineDevice(unsigned int devBitMap)
+{
+		int j = 0; 													/* loop control variable for determining interrupt device number */
+		unsigned int deviceOn = DEVICE0;							/* indicates an interrupt on device i */
+		unsigned int interruptOn;
+		int foundDevice = FALSE; 									/* true if the interrupting device has been found, false otherwise */
+		int devNum;													/* highest priority device with interrupt */
+
+		/* find highest priority device with interrupt */
+		while(j < NUMDEVICES && !foundDevice)
+		{
+			devBitMap = devBitMap & MASKDEVBITMAP; 					/* zero out irrelevant bits */
+			interruptOn = devBitMap & deviceOn; 					/* all 0s if there is NOT an interrupt on device i */
+
+			/* case: device i has an interrupt */
+			if(interruptOn != 0)
+			{
+				foundDevice = TRUE;
+				devNum = j;
+			}
+			j++; 													/* check the next device */
+			deviceOn = deviceOn << 1;
+		}
+
+	return devNum;
+}
+
+HIDDEN void PLTInterruptHandler()
+{
+	/* end the current process's turn */
+	IncrementProcessTime(currentProcess);
+	insertProcQ(&readyQ, currentProcess);
+	Scheduler();												/* schedule a new process to run */
+}
+
+HIDDEN void ITInterruptHandler()
+{
+	int* sema4 = &(semaphoreArray[ITSEMINDEX]);					/* interval timer's semaddr */
+	pcb_PTR p;
+	/* unblock all the processes that were waiting for the clock */
+	while((*sema4) < 0)
+	{
+		(*sema4)++;
+		p = removeBlocked(sema4);
+		insertProcQ(&readyQ, p);
+	}
+	(*sema4) = 0;
+	LDIT(INTERVALTIME);
+}
+
+HIDDEN void ExitInterruptHandler(state_PTR interruptOld)
+{
+	cpu_t INTERRUPTEND;												/* end time of interrupt handler */
+	/* case: there was no process that got interrupted */
+	if(currentProcess == NULL)
+	{
+		Scheduler();
+	}											/* schedule a new process to run */
+	/* case: a process got interrupted */
+	else
+	{
+		STCK(INTERRUPTEND);
+		LoadState(interruptOld); 									/* return control to the interrupted process*/
+	}
+
 }
